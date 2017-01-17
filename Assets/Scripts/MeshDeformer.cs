ea7eb8e909f38a9c948ac08b0bf705a1bda233e9
Vinícius <vncastanheira@gmail.com>
2017-01-17 01:13:38 -0200

    Soft mesh deformation

---
+++ Assets/Scripts/MeshDeformer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshDeformer : MonoBehaviour
+{
+    public int deformingIterations;
+
+    Mesh deformingMesh;
+    MeshCollider meshCollider;
+    Vector3[] originalVertices, displacedVertices;
+    Vector3[] vertexVelocities;
+
+    void Start()
+    {
+        meshCollider = GetComponent<MeshCollider>();
+        deformingMesh = GetComponent<MeshFilter>().mesh;
+        originalVertices = displacedVertices = deformingMesh.vertices;        
+        vertexVelocities = new Vector3[originalVertices.Length];
+    }
+
+    public void AddDeformingForce(Vector3 point, float force)
+    {
+        for (int i = 0; i < displacedVertices.Length; i++)
+        {
+            AddForceToVertex(i, point, force);
+        }
+        StartCoroutine(DeformingIteration());
+        meshCollider.sharedMesh = deformingMesh;
+    }
+
+    IEnumerator DeformingIteration()
+    {
+        for (int i = 0; i < deformingIterations; i++)
+        {
+            for (int vertex = 0; vertex < displacedVertices.Length; vertex++)
+            {
+                UpdateVertex(vertex);
+            }
+            deformingMesh.vertices = displacedVertices;
+            deformingMesh.RecalculateNormals();
+            yield return null;
+        }
+    }
+
+    private void AddForceToVertex(int i, Vector3 point, float force)
+    {
+        Vector3 pointToVertex = displacedVertices[i] - point;
+        float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
+        float velocity = attenuatedForce * Time.deltaTime;
+        vertexVelocities[i] += pointToVertex.normalized * velocity;
+    }
+
+    private void UpdateVertex(int i)
+    {
+        Vector3 velocity = vertexVelocities[i];
+        displacedVertices[i] += velocity * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/MeshDeformerCollision.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshDeformer))]
+public class MeshDeformerCollision : MonoBehaviour
+{
+    public float force = 10f;
+    public float forceOffset = 0.1f;
+    bool isBroken = false;
+
+    MeshDeformer deformer;
+
+	void Start ()
+    {
+        deformer = GetComponent<MeshDeformer>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!isBroken)
+        {
+            foreach (var contact in collision.contacts)
+            {
+                var point = contact.point;
+                point += contact.normal * forceOffset;
+                deformer.AddDeformingForce(contact.point, force);
+            }
+            isBroken = true;   
+        }
+    }
+}
